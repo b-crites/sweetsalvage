2d1f0776bd432c18c87da751793bfcb8e134f816
Brandon <info@visonaryadvance.com>
2024-10-09 18:32:30 -0700

    first

--- app/components/Footer.jsx
+++ app/components/Footer.jsx
@@ -1,9 +1,131 @@
-import React from 'react'
+import React from "react";
+import { FaFacebook } from "react-icons/fa";
+import { FaInstagram } from "react-icons/fa";
 
 function Footer() {
   return (
-    <div>Footer</div>
-  )
+    <>
+      <footer class="relative bg-blueGray-200 pt-8 pb-6">
+        <div class="container mx-auto px-4">
+          <div class="flex flex-wrap text-left lg:text-left">
+            <div class="w-full lg:w-6/12 px-4">
+              <h4 class="text-3xl fonat-semibold text-blueGray-700">
+                Let's keep in touch!
+              </h4>
+
+              <div className="mt-6 lg:mb-0 mb-6 flex">
+  <button
+    className="bg-white text-lightBlue-400 shadow-lg font-normal h-10 w-10 flex items-center justify-center rounded-full outline-none focus:outline-none mr-2"
+    type="button"
+  >
+    <FaFacebook className="text-4xl" />
+  </button>
+  <button
+    className="bg-white text-lightBlue-600 shadow-lg font-normal h-10 w-10 flex items-center justify-center rounded-full outline-none focus:outline-none mr-2"
+    type="button"
+  >
+    <FaInstagram className="text-4xl" />
+  </button>
+ 
+</div>
+
+            </div>
+            <div class="w-full lg:w-6/12 px-4">
+              <div class="flex flex-wrap items-top mb-6">
+                <div class="w-full lg:w-4/12 px-4 ml-auto">
+                  <span class="block uppercase text-blueGray-500 text-sm font-semibold mb-2">
+                    Useful Links
+                  </span>
+                  <ul class="list-unstyled">
+                    <li>
+                      <a
+                        class="text-blueGray-600 hover:text-blueGray-800 font-semibold block pb-2 text-sm"
+                        href="https://www.creative-tim.com/presentation?ref=njs-profile"
+                      >
+                        About Us
+                      </a>
+                    </li>
+                    <li>
+                      <a
+                        class="text-blueGray-600 hover:text-blueGray-800 font-semibold block pb-2 text-sm"
+                        href="https://blog.creative-tim.com?ref=njs-profile"
+                      >
+                        Blog
+                      </a>
+                    </li>
+                    <li>
+                      <a
+                        class="text-blueGray-600 hover:text-blueGray-800 font-semibold block pb-2 text-sm"
+                        href="https://www.github.com/creativetimofficial?ref=njs-profile"
+                      >
+                        Website
+                      </a>
+                    </li>
+                    <li>
+                      <a
+                        class="text-blueGray-600 hover:text-blueGray-800 font-semibold block pb-2 text-sm"
+                        href="https://www.creative-tim.com/bootstrap-themes/free?ref=njs-profile"
+                      >
+                        Free Products
+                      </a>
+                    </li>
+                  </ul>
+                </div>
+                <div class="w-full lg:w-4/12 px-4">
+                  <span class="block uppercase text-blueGray-500 text-sm font-semibold mb-2">
+                    Other Resources
+                  </span>
+                  <ul class="list-unstyled">
+                    <li>
+                      <a
+                        class="text-blueGray-600 hover:text-blueGray-800 font-semibold block pb-2 text-sm"
+                        href="https://github.com/creativetimofficial/notus-js/blob/main/LICENSE.md?ref=njs-profile"
+                      >
+                        MIT License
+                      </a>
+                    </li>
+                    <li>
+                      <a
+                        class="text-blueGray-600 hover:text-blueGray-800 font-semibold block pb-2 text-sm"
+                        href="https://creative-tim.com/terms?ref=njs-profile"
+                      >
+                        Terms &amp; Conditions
+                      </a>
+                    </li>
+                    <li>
+                      <a
+                        class="text-blueGray-600 hover:text-blueGray-800 font-semibold block pb-2 text-sm"
+                        href="https://creative-tim.com/privacy?ref=njs-profile"
+                      >
+                        Privacy Policy
+                      </a>
+                    </li>
+                    <li>
+                      <a
+                        class="text-blueGray-600 hover:text-blueGray-800 font-semibold block pb-2 text-sm"
+                        href="https://creative-tim.com/contact-us?ref=njs-profile"
+                      >
+                        Contact Us
+                      </a>
+                    </li>
+                  </ul>
+                </div>
+              </div>
+            </div>
+          </div>
+          <hr class="my-6 border-blueGray-300" />
+          <div class="flex flex-wrap items-center md:justify-between justify-center">
+            <div class="w-full md:w-4/12 px-4 mx-auto text-center">
+              <div class="text-sm text-blueGray-500 font-semibold py-1">
+                All Rights Reserved <span id="get-current-year">2024</span>{" "}
+                Sweet Salvage
+              </div>
+            </div>
+          </div>
+        </div>
+      </footer>
+    </>
+  );
 }
 
-export default Footer
+export default Footer;
--- app/page.js
+++ app/page.js
@@ -1,63 +1,190 @@
+"use client";
 
-
+import "boxicons";
 
 export default function Home() {
+  const events = [
+    {
+      date: "18",
+      day: "FRI",
+      month: "OCT",
+      name: "Luke Bryan",
+      description: "This is where Luke Bryan will be on this date",
+    },
+    {
+      date: "20",
+      day: "SUN",
+      month: "OCT",
+      name: "Luke Bryan",
+      description: "This is where Luke Bryan will be on this date",
+    },
+    {
+      date: "26",
+      day: "SAT",
+      month: "OCT",
+      name: "Luke Bryan",
+      description: "This is where Luke Bryan will be on this date",
+    },
+  ];
 
-const events = [
-  {date:"18", day:"FRI", month:"OCT", name: "Luke Bryan", description:"This is where Luke Bryan will be on this date"},
-  {date:"20", day:"SUN", month:"OCT", name: "Luke Bryan", description:"This is where Luke Bryan will be on this date"},
-  {date:"26", day:"SAT", month:"OCT", name: "Luke Bryan", description:"This is where Luke Bryan will be on this date"}
-]
+  const food = [
+    { img: "/Img/tailg8s.jpg", name: "Tailg8's", url: "https://tailg8s.com" },
+    {
+      img: " /Img/Calle_Steelo_Logo.jpg",
+      name: "Calle Steelo Taqueria",
+      url: "https://www.facebook.com/CalleSteeloTaqueria/",
+    },
+    {
+      img: "/Img/Kentos.jpg",
+      name: "Kento",
+      url: "https://kentosfoodtruck.com/",
+    },
+    {
+      img: "/Img/once.webp",
+      name: "Once Famous Grill",
+      url: "https://www.oncefamousgrill.com/",
+    },
+  ];
 
-
-
+  const posts = [
+    { img: "/Img/yard.jpg", alt: "Sweet Salvage" },
+    { img: "/Img/Fuller.jpg", alt: "James Fuller" },
+    { img: "/Img/Cramer Boys.png", alt: "Cramer Boys" },
+    { img: "/Img/cramer boys setup.png", alt: "Cramer Boys Setup" },
+    { img: "/Img/Ghosts.png", alt: "Ghosts" },
+    { img: "/Img/In Line.png", alt: "People In Line" },
+    { img: "/Img/Northern Lights.png", alt: "Northern Lights Scenery" },
+    { img: "/Img/Store.png", alt: "Store" },
+    { img: "/Img/View.png", alt: "View of Yard" },
+  ];
 
   return (
- <>
- <div className="">
-  {/* LANDING IMG */}
-  <div className=" h-96 bg-stone-500 w-full">
-    <h2>TITLE</h2>
+    <>
+      <div className="">
+        {/* LANDING IMG */}
+        <div className="relative h-96 w-full">
+  <img className="h-full w-full object-cover" src="/Img/Farm.jpg" alt="" />
+  <div className="absolute inset-0 bg-black bg-opacity-50 flex justify-center items-center">
+    <h1 className="text-6xl font-semibold font-serif text-white">SWEET SALVAGE</h1>
   </div>
-{/* ================================================= */}
-{/* Events Section */}
-<div className="w-full">
-  <div className=" text-center text-3xl font-semibold font-serif pt-5">
-    <h3 className="">Upcoming Events</h3>
-  </div>
-  <div className="pt-10 md:w-1/2 mx-auto">
-    {/* Card Start */}
-    {events.map((item,index) => (
-    
-    <div key={index} className="grid grid-cols-6 shadow-xl bg-gray-50 rounded-xl mb-8">
-      <div className="col-span-1 text-center text-white py-2 bg-red-500 rounded-l-xl ">
-      <h4 className="">{item.month}</h4>
-      <h4 className="text-4xl font-semibold">{item.date}</h4>
-      </div>
-      <div className="col-span-5 ms-5">
-        <h3 className=" font-bold text-2xl  ">
-          {item.name}
-        </h3>
-        <p>{item.description}</p>
+</div>
+
+
+        {/* ================================================= */}
+        {/* Events Section */}
+        <div className="w-full">
+          <div className=" text-center text-3xl font-semibold font-serif pt-5">
+            <h3 className="">Upcoming Events</h3>
+          </div>
+          <div className="pt-10 md:w-1/2 mx-auto">
+            {/* Card Start */}
+            {events.map((item, index) => (
+              <div
+                key={index}
+                className="grid grid-cols-6 shadow-xl bg-gray-50 rounded-xl mb-8"
+              >
+                <div className="col-span-1 text-center text-white py-2 bg-red-500 rounded-l-xl ">
+                  <h4 className="">{item.month}</h4>
+                  <h4 className="text-4xl font-semibold">{item.date}</h4>
+                </div>
+                <div className="col-span-5 ms-5">
+                  <h3 className=" font-bold text-2xl  ">{item.name}</h3>
+                  <p>{item.description}</p>
+                </div>
+              </div>
+            ))}
+            <div className=" pt-10 center">
+              <button className="rounded-xl py-2 px-6 text-xl bg-red-500 text-white">
+                See All
+              </button>
+            </div>
+          </div>
         </div>
+        {/* ==================================================================================================== */}
+        {/* Start Food Section */}
+        <div className="">
+          <div className="pt-20 text-center text-3xl font-semibold font-serif">
+            <h3 className="">Food Trucks</h3>
+          </div>
+          <div className="grid grid-cols-2 w-1/2 mx-auto mt-10">
+            {food.map((item, index) => (
+              <div key={index} className="relative mx-auto w-72 mb-20">
+                <a href={item.url} target="_blank" rel="noopener noreferrer">
+                  <img
+                    src={item.img}
+                    className="w-full h-auto"
+                    alt={item.name}
+                  />
 
-    </div>
-    
-    ))}
-    <div className=" pt-10 center">
-      <button className="rounded-xl py-2 px-6 text-xl bg-red-500 text-white">See All</button>
-    </div>
+                  {/* Hover effect */}
+                  <div className="absolute inset-0 bg-black bg-opacity-50 flex justify-center items-center opacity-0 hover:opacity-100 transition-opacity duration-300">
+                    <p className="text-white text-lg font-bold">{item.name}</p>
+                  </div>
+                </a>
+              </div>
+            ))}
+          </div>
+        </div>
+      </div>
+      {/* ======================================================================================================== */}
+      {/* About Section */}
+      <div className="mt-20">
+        
+      <div className="grid mx-auto mt-20 grid-cols-2 w-8/12">
+  <div className="col-span-1 mx-auto">
+    <img
+      src="/Img/Sweet Salvage Store.png"
+      className="mx-auto bg-black"
+      alt="Picture"
+    />
   </div>
+  <div className="col-span-1 mx-auto flex flex-col justify-center text-center">
+    <h3 className="text-center font-bold text-5xl font-serif mb-4">About Us</h3>
+    <p className="mx-10">
+      Welcome to Sweet Salvage – your go-to spot for cool, vintage vibes
+      and good times! Inside, you’ll find a mix of farmhouse decor,
+      unique finds, and all the cozy stuff to make your space feel
+      special. But we’re more than just a shop – outside, our food truck
+      pavilion has all the tasty eats, and we’ve got live music to keep
+      things lively. Whether you're here to shop, grab a bite, or just
+      hang out, Sweet Salvage is all about laid-back fun and making
+      memories.
+    </p>
+  </div>
+</div>
 
+      </div>
+      {/* ======================================================================================================================= */}
+      {/* IG POSTS */}
+      <div className="font-bold text-3xl font-serif text-center mt-20">
+  <h3 className="flex justify-center items-center gap-2">
+    Follow Us on
+    <a href="https://www.instagram.com/sweetsalvagedesigns/?hl=en" target="_blank" rel="noopener noreferrer">
+    <box-icon type="logo" size='lg' name="instagram" />
+    </a>
+    /
+    <a href="https://www.facebook.com/SweetSalvageDesigns/" target="_blank" rel="noopener noreferrer">
+    <box-icon name="facebook" size='lg' type="logo"></box-icon>
+    </a>
+  </h3>
 </div>
-{/* ==================================================================================================== */}
-{/* Start Food Section */}
-<div className="">
-<div className="pt-20 text-center text-3xl font-semibold font-serif">
-    <h3 className="">Food Trucks</h3>
-  </div>
+
+<div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 gap-4 max-w-6xl mx-auto my-8">
+  {posts.map((post, index) => (
+    <div key={index} className="relative">
+      <img 
+        src={post.img} 
+        alt={post.alt} 
+        className="w-full h-[300px] object-cover" // Adjust height as needed
+      />
+      {/* Optional hover effect */}
+      <div className="absolute inset-0 bg-black bg-opacity-50 flex justify-center items-center opacity-0 hover:opacity-100 transition-opacity duration-300">
+        <p className="text-white text-lg font-bold">{post.alt}</p>
+      </div>
+    </div>
+  ))}
 </div>
- </div>
- </>
+
+    </>
   );
 }
