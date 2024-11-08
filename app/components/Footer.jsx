dfeb6d9079060e21674a82f38e925654554bd81a
Brandon <brandoncrites76@gmail.com>
2024-11-07 22:02:09 -0800

    Band page done, about page done, header fixed to group pavillion tabs

--- app/components/Footer.jsx
+++ app/components/Footer.jsx
@@ -5,7 +5,7 @@ import { FaInstagram } from "react-icons/fa";
 function Footer() {
   return (
     <>
-      <footer className="relative bg-blueGray-200 pt-8 pb-6">
+      <footer className=" relative bg-blueGray-200 pt-8 pb-6">
         <div className="container mx-auto px-4">
           <div className="flex flex-wrap text-left lg:text-left">
             <div className="w-full lg:w-6/12 px-4">
